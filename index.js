5eb4bc74c7bfdbab547e680120c6d3ef603d4013
sabirabanu94 <sabirabanu94@gmail.com>
2025-02-16 20:51:41 +0530

    second commit

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 // Define routes here
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
